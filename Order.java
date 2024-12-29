0c7634d8e72dff03611be0b7ad7573b1b1e87751
MazinElhag <108403482+MazinElhag@users.noreply.github.com>
2024-12-30 00:10:50 +0200

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,16 +1,17 @@
 package Library_Manager;
 
 public class Order {
-    int order_id = 0;
+    int order_id;
     String userName, bookTitle;
     long bookCode;
 
-    public Order(String userName, String bookTitle, long bookCode) {
+    public Order(int order_id,String userName, String bookTitle, long bookCode) {
         this.userName = userName;
         this.bookTitle = bookTitle;
         this.bookCode = bookCode;
-        order_id++;
+        this.order_id = order_id;
     }
+
 
     public int getOrder_id() {
         return order_id;
